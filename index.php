7c36e133cb54b74b543175826740b7cafc43dcd5
Johnnie Munger <johnnietheblack@gmail.com>
2014-08-08 20:50:31 -0700

    added monolog

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 session_start();
 
-// Start Dominos - vroom
-require 'src/autoload.php';
+require 'vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+// add records to the log
+$log->addWarning("Running PHP!");
+
 $dominos = new \Dominos\Dominos(true); // TRUE | FALSE for test mode
 
 if(isset($_POST['sub_order'])) {
